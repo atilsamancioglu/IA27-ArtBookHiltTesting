5131dc083a52d915ce83fb83e8f162e22827ca92
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2021-01-12 10:23:52 +0300

    Completed ArtBook with tests

--- app/src/main/java/com/atilsamancioglu/artbookhilttesting/util/Util.kt
+++ app/src/main/java/com/atilsamancioglu/artbookhilttesting/util/Util.kt
@@ -2,7 +2,7 @@ package com.atilsamancioglu.artbookhilttesting.util
 
 object Util {
 
-    const val API_KEY = "19822537-4941426fa307c04e49965bd05"
+    const val API_KEY = "Replace with your own API Key"
     const val BASE_URL = "https://pixabay.com"
 
 }
